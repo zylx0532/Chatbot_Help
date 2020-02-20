263ef480ffd6c3ac5803105501cfa3f312b2feaf
Blood.Cold <webmaster@zhiqiang.name>
2020-02-21 00:52:42 +0800

    完善并尝试增加独立配置文件

---
+++ demo/config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------- 
+# @CreateTime   : 2020/2/21 0:10
+# @Author       : WaterIce
+# @Email        : waterice@vip.qq.com
+# @Version      : 1.0 
+# @File         : config.py 
+# @Desc         : 
+# ------------------------------------
+
+import os
+import configparser
+
+class Config(object):
+    def __init__(self, config_file='config.ini'):
+        self._path = os.path.join(os.getcwd(), config_file)
+        if not os.path.exists(self._path):
+            raise FileNotFoundError("No such file: config.ini")
+        self._config = configparser.ConfigParser()
+        self._config.read(self._path, encoding='utf-8-sig')
+        self._configRaw = configparser.RawConfigParser()
+        self._configRaw.read(self._path, encoding='utf-8-sig')
+
+    def get(self, section, name):
+        return self._config.get(section, name)
+
+    def getRaw(self, section, name):
+        return self._configRaw.get(section, name)
